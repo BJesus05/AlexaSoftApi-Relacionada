c545a9a707b89857241615d8fc5c352127cd02f4
Brilis Vergara <brilisdejesus.v.l@gmail.com>
2023-11-18 15:25:01 -0500

    citas

--- server/router/tasks.router.js
+++ server/router/tasks.router.js
@@ -8,26 +8,41 @@ router.get('/tasks', async(req,res) => {
     res.json(result)
 });
 router.get('/tasks/:id' , async (req,res) => {
-    const [result] = await Pool.query("Select * from tasks where id = ?", [req.params.id]);
-    console.log(result)
-    if (result.length === 0) {
-        res.status(404).json({mensaje: "tarea no encontrada"})
-    }
-    res.json(result)
+   try {
+        const [result] = await Pool.query("Select * from tasks where id = ?", [req.params.id]);
+        console.log(result)
+        if (result.length === 0) {
+            res.status(404).json({mensaje: "tarea no encontrada"})
+        }
+        res.json(result)
+   } catch (error) {
+    return res.status(500).json({ message: error.message})
+
+   }
 });
 router.post('/tasks', async (req,res) => {
-    const {titulo, descripcion} =req.body;
-    const [result] =  await Pool.query ("INSERT INTO tasks(titulo, descripcion) VALUES (?,?)", [titulo, descripcion]);
-    res.json({
-        id: result.insertId,
-        titulo,
-        descripcion
-    })
+    try {
+        const {titulo, descripcion} =req.body;
+        const [result] =  await Pool.query ("INSERT INTO tasks(titulo, descripcion) VALUES (?,?)", [titulo, descripcion]);
+        res.json({
+            id: result.insertId,
+            titulo,
+            descripcion
+        })
+    } catch (error) {
+        return res.status(500).json({ message: error.message})
+
+    }
 });
 router.put('/tasks/:id', async(req,res) => {
-    const {titulo, descripcion} =req.body;
-    const [result] = await Pool.query("update tasks set ? where id = ?", [req.body,req.params.id]);
+   try {
+        const {titulo, descripcion} =req.body;
+        const [result] = await Pool.query("update tasks set ? where id = ?", [req.body,req.params.id]);
     res.json(result)
+   } catch (error) {
+    return res.status(500).json({ message: error.message})
+
+   }
 });
 router.delete('/tasks/:id', async(req,res) => {
     try {
@@ -39,7 +54,8 @@ router.delete('/tasks/:id', async(req,res) => {
          res.sendStatus(204) 
     }
     } catch (error) {
-         
+        return res.status(500).json({ message: error.message})
+ 
 }
 });
 
